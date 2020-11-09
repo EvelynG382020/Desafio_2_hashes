8dc9ac4d4198dfbdbdb6a53482808d6581966890
evelyn gomez <evelyngomezfrez@gmail.com>
2020-11-08 21:40:59 -0300

    desafio 2 hashes

--- busqueda.rb
+++ busqueda.rb
@@ -6,32 +6,28 @@
 #Se evaluará con un hash distinto al mostrado.
 
 #ruby busqueda.rb 15000 31000 21700
-def months(valor)
-    ventas = {
-        Enero: 15000,
-        Febrero: 22000,
-        Marzo: 12000,
-        Abril: 17000,
-        Mayo: 81000,
-        Junio: 13000,
-        Julio: 21000,
-        Agosto: 41200,
-        Septiembre: 25000,
-        Octubre: 21500,
-        Noviembre: 91000,
-        Diciembre: 21000
-    }
-    search = valor.map {|x| x.to_i} #en mi arreglo hay que recorrer
-    #cada elemento y transformarlo a integer
-    n = search.size
-    n.times do |x|
-        if ventas.invert[search[x]]
-            print ventas.invert[search[x]]
-        else
-            print "No encontrado"
-        end
-        print " "
-    end
-end
+ventas = {
+    Enero: 15000,
+    Febrero: 22000,
+    Marzo: 12000,
+    Abril: 17000,
+    Mayo: 81000,
+    Junio: 13000,
+    Julio: 21000,
+    Agosto: 41200,
+    Septiembre: 25000,
+    Octubre: 21500,
+    Noviembre: 91000,
+    Diciembre: 21000
+}
 
-months(ARGV)
+ARGV.each do |busqueda| 
+    found =  ventas.invert[busqueda.to_i]
+        if found
+            print " #{found} "
+        else
+        print " no encontrado"
+        end
+    end
+
+
--- trimestres.rb
+++ trimestres.rb
@@ -6,3 +6,28 @@
 #Es un ejercicio de arrays o de hashes ¿Se necesitan los keys?
 #Revisar la documentación del método .each_slice
 #Necesitamos iterar elementos, o elementos con índices
+
+ventas_trimestrales = []
+i = 0
+
+ventas.each do |trimestre|
+    hash_quarters = trimestre.to_h
+    valores_trimestrales = hash_quarters.values #values retorna nuevo array con los valores desde el hash
+    ventas_trimestrales[i] = ventas_trimestrales.inject(0) {|suma, venta| suma+venta}
+    #Si especifico un bloque, entonces para cada elemento en enum, el bloque se pasa un valor acumulador 
+    i += 1
+end
+
+
+quarters = ["Q1", "Q2", "Q3", "Q4"] (ventas_trimestrales).to_h    
+print quarters
+
+
+
+
+
+
+
+
+
+
